bd4eb82d42f0f33adb807a3aafef26477da90ef3
jun-hf <fong.junh@gmail.com>
2021-11-23 02:13:23 +0000

    added read and no read functionality, ability to submit new book

--- Library/index.js
+++ Library/index.js
@@ -1,7 +1,13 @@
 const container_list = document.querySelector(".container-list");
-console.dir(container_list.firstElementChild);
+const addForm = document.querySelector("#addForm");
+const submitBtn = document.querySelector("#submit");
 
-function Book(title = "hp", author = "jk", pages = "100", read = "read") {
+function Book(
+  title = "Harry Potter",
+  author = "J,K",
+  pages = "300",
+  read = "read"
+) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -10,10 +16,14 @@ function Book(title = "hp", author = "jk", pages = "100", read = "read") {
 
 Book.prototype.updateRead = function () {
   if (this.read === "read") {
-    this.read = "unread";
+    this.read = "no read";
+    console.log(this.read);
   } else {
     this.read = "read";
+    console.log(this.read);
   }
+  clearContainer();
+  display(myLibrary);
 };
 
 let myLibrary = [];
@@ -23,6 +33,12 @@ const b2 = new Book();
 
 myLibrary.push(b1);
 myLibrary.push(b2);
+
+// clear container
+const clearContainer = () => {
+  const container_list = document.querySelector(".container-list");
+  container_list.innerHTML = "";
+};
 
 // create a function that will re-render myLibrary
 const display = (arr) => {
@@ -48,16 +64,25 @@ const display = (arr) => {
 
     const readButton = document.createElement("button");
     readButton.textContent = "read";
-    readButton.classList.add("read-btn");
     readButton.addEventListener("click", () => arr[i].updateRead());
     div.appendChild(readButton);
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "delete";
-    div.appendChild(deleteButton);
 
     container_list.appendChild(div);
   }
 };
 
 display(myLibrary);
+
+const addBook = () => {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").value;
+
+  addForm.reset();
+  myLibrary.push(new Book(title, author, pages, read));
+  clearContainer();
+  display(myLibrary);
+};
+
+submitBtn.addEventListener("click", () => addBook());
